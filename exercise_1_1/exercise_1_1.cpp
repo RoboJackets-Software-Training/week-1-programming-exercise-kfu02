ebb0fd30bf93e0f66016d8e4ece3c2611cdd42e1
kfu02 <kevin.fu842@gmail.com>
2020-09-19 22:05:05 -0400

    missing colon

--- exercise_1_1/exercise_1_1.cpp
+++ exercise_1_1/exercise_1_1.cpp
@@ -49,7 +49,7 @@ int main() {
 		}
 
 		for (int i = 0; i < 4; i++) {
-			std::cout << "wheel at index " << i << " has speed " << wheel_speeds[i] << std::endl;
+			std::cout << "wheel at index " << i << " has speed: " << wheel_speeds[i] << std::endl;
 		}
         //
     }
